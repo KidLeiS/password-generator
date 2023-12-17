62ee54a36b159fbfe5df29559a76d716bea6702d
Kid Lei <eric@collectiveequity.com>
2023-12-17 10:03:13 +0000

    written javascript for password generator

--- script.js
+++ script.js
@@ -88,19 +88,80 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+var passReqs = {
+  passLen: 0,
+  lenVal: false,
+  characters: [],
+  charVal: false,
+};
+
+function resetPassReqs() {
+  passReqs.lenVal = false;
+  passReqs.charVal = false;
+  passReqs.characters = [];
+}
+
 // Function to prompt user for password options
 function getPasswordOptions() {
+  passReqs.passLen = prompt("Please input the length of your password: ");
+  if (passReqs.passLen > 7 && passReqs.passLen < 129 && passReqs.passLen % 1 == 0) {
+    passReqs.lenVal = true;
+  };  
+  while (!passReqs.lenVal) {
+    passReqs.passLen = prompt("Please input the length of your password (between 8 and 128 characters): ");
+      
+    if (passReqs.passLen > 7 && passReqs.passLen < 129 && passReqs.passLen % 1 == 0) {
+      passReqs.lenVal = true;
+    };
+  };
+  
 
+  while (!passReqs.charVal) {
+    var contLower = confirm("Do you want your password to contain lowercase letters?");
+    if (contLower) {
+      passReqs.characters = passReqs.characters.concat(lowerCasedCharacters);
+    }
+
+    var contUpper = confirm ("Do you want your password to contain uppercase letters?");
+    if (contUpper) {
+      passReqs.characters = passReqs.characters.concat(upperCasedCharacters);
+    }
+
+    var contNumb = confirm ("Do you want your password to contain numbers?");
+    if (contNumb) {
+      passReqs.characters = passReqs.characters.concat(numericCharacters);
+    }
+
+    var contSpec = confirm ("Do you want your password to contain special characters ($@%&*, etc)?");
+    if (contSpec) {
+      passReqs.characters = passReqs.characters.concat(specialCharacters);
+    }
+
+    if (passReqs.characters.length > 0) {
+      passReqs.charVal = true;
+    } else {
+      alert("Please select at least one character type");
+    }
+    
+  };
+  
+  console.log(passReqs.characters);
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  var i = Math.floor(Math.random() * arr.length);
+  return arr[i];
 }
 
 // Function to generate password with user input
 function generatePassword() {
-
+  var password = "";
+  for (var i = 0; i < passReqs.passLen; i++) {
+    password = password + getRandom(passReqs.characters).toString();
+  };
+  console.log(password);
+  return password;
 }
 
 // Get references to the #generate element
@@ -108,10 +169,14 @@ var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
+  getPasswordOptions();
+
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
+
+  resetPassReqs();
 }
 
 // Add event listener to generate button
